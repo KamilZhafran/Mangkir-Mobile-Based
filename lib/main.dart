6a3049a3e71975d75c6932bc284989459b20a9d3
BgsHryd <76993913+BgsHryd@users.noreply.github.com>
2023-05-18 10:31:49 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:tubes_app/Home.dart';
+import 'package:tubes_app/Login.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
+    return MaterialApp(
+      title: 'Hello, World!',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HelloWorldScreen(),
+    );
+  }
+}
+
+class HelloWorldScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello, World!'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),
+            );
+          },
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
   }
 }
