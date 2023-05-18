eb99f335bf847299c54f57533d83a4cc050a2690
ehangehang <rayhansuryatama@gmail.com>
2023-05-18 19:41:01 +0700

    main bottomNav & Home Page layout

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tubes_app/Login.dart';
 
 void main() {
   runApp(const Home());
@@ -25,61 +27,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int selected = 0;
-  PageController pc = PageController();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: SafeArea(
-        child: PageView(
-          controller: pc,
-          onPageChanged: (value) {
-            setState(() {
-              selected = value;
-            });
-          },
-          children: [
-            Container(
-              child: Text('Home'),
-            ),
-            Container(
-              child: Text('Upload'),
-            ),
-            Container(
-              child: Text('Favorite'),
-            ),
-          ],
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(height: 200.0),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(color: Colors.amber),
+                  child: Center(
+                    child: Text(
+                      '$i',
+                      style: const TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                );
+              },
+            );
+          }).toList(),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selected,
-        onTap: (value) {
-          setState(() {
-            selected = value;
-            pc.animateToPage(value,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeIn);
-          });
-        },
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: new SvgPicture.asset('assets/images/ic_round-home.svg'),
-            label: 'Home',
+        Center(
+          child: Text(
+            'Others for You!',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: new SvgPicture.asset('assets/images/ic_cloud-upload.svg'),
-            label: 'Upload',
+        ),
+        Expanded(
+          child: GridView.count(
+            crossAxisCount: 3,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            padding: const EdgeInsets.all(20),
+            children: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) {
+              return Builder(builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(color: Colors.teal),
+                  child: Center(
+                    child: Text(
+                      '$i',
+                      style: const TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                );
+              });
+            }).toList(),
           ),
-          BottomNavigationBarItem(
-            icon: new SvgPicture.asset('assets/images/ic_cards-heart.svg'),
-            label: 'Favorite',
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tubes_app/Home.dart';
 import 'package:tubes_app/Login.dart';
+
+import 'Home.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,31 +17,71 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HelloWorldScreen(),
+      home: BottomNav(),
     );
   }
 }
 
-class HelloWorldScreen extends StatelessWidget {
+class BottomNav extends StatefulWidget {
+  const BottomNav({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNav> createState() => _BottomNav();
+}
+
+class _BottomNav extends State<BottomNav> {
+  int selected = 0;
+  PageController pc = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello, World!'),
+        title: const Text('Home'),
       ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LoginPage()),
-            );
+      body: SafeArea(
+        child: PageView(
+          controller: pc,
+          onPageChanged: (value) {
+            setState(() {
+              selected = value;
+            });
           },
-          child: Text(
-            'Hello, World!',
-            style: TextStyle(fontSize: 24),
-          ),
+          children: [
+            HomePage(),
+            Container(
+              child: Text('Upload'),
+            ),
+            Container(
+              child: Text('Favorite'),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selected,
+        onTap: (value) {
+          setState(() {
+            selected = value;
+            pc.animateToPage(value,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeIn);
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/images/ic_round-home.svg'),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/images/ic_cloud-upload.svg'),
+            label: 'Upload',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/images/ic_cards-heart.svg'),
+            label: 'Favorite',
+          ),
+        ],
       ),
     );
   }
