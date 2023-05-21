7432c50c060856502d34fb680c23f6a54c6f8f0b
kamil zhafran <zhafrankamil17@gmail.com>
2023-05-21 23:02:35 +0700

    Recipe Page

--- lib/Recipe.dart
+++ lib/Recipe.dart
@@ -63,7 +63,24 @@ class Ingredients extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // kerjain di sini
-    return Text("Ingredients");
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Expanded(
+        child:
+          ListView.builder(
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+                leading: const Icon(Icons.list),
+                trailing: const Text(
+                  "GFG",
+                  style: TextStyle(color: Colors.green, fontSize: 15),
+                ),
+                title: Text("List item $index"));
+          }),
+      )
+        
+    );
   }
 }
 
