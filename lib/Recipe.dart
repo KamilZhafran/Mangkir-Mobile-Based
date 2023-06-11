e1b44414a461f8bebf098202ba70056a01390a3c
BgsHryd <76993913+BgsHryd@users.noreply.github.com>
2023-06-11 11:28:25 +0700

    Refactor Recipe.dart and fetch for Home
    1. Fetch from back end to home.dart
    2. refactor of Recipe.dart into RecipePage.dart
    3. added Recipe object .dart file to easier navigation and storing variable

--- lib/Recipe.dart
+++ lib/Recipe.dart
@@ -1,105 +1,34 @@
-// create a blank screen here with state
+class Recipe {
+  final int id;
+  final String email;
+  final String judul;
+  final String backstory;
+  final String asal;
+  final int porsi;
+  final int durasi;
+  final String kategori;
 
-import 'package:flutter/material.dart';
+  const Recipe({
+    required this.id,
+    required this.judul,
+    required this.email,
+    required this.backstory,
+    required this.asal,
+    required this.porsi,
+    required this.durasi,
+    required this.kategori,
+  });
 
-void main() {
-  runApp(const Recipe());
-}
-
-class Recipe extends StatefulWidget {
-  const Recipe({Key? key}) : super(key: key);
-
-  @override
-  _RecipeState createState() => _RecipeState();
-}
-
-class _RecipeState extends State<Recipe> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 180,
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  text: 'FIRST',
-                ),
-                Tab(
-                  text: 'SECOND',
-                ),
-                Tab(text: 'THIRD'),
-              ],
-            ),
-            title: Container(
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/kill.jpg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Ingredients(),
-              Instruction(),
-              Comments(),
-            ],
-          ),
-        ),
-      ),
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      id: json['recipeID'],
+      email: json['emailAuthor'],
+      judul: json['judul'],
+      backstory: json['backstory'],
+      asal: json['asalDaerah'],
+      porsi: json['servings'],
+      durasi: json['durasi_menit'],
+      kategori: json['kategori'],
     );
-  }
-}
-
-class Ingredients extends StatelessWidget {
-  const Ingredients({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // kerjain di sini
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Expanded(
-        child:
-          ListView.builder(
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-                leading: const Icon(Icons.list),
-                trailing: const Text(
-                  "GFG",
-                  style: TextStyle(color: Colors.green, fontSize: 15),
-                ),
-                title: Text("List item $index"));
-          }),
-      )
-        
-    );
-  }
-}
-
-class Instruction extends StatelessWidget {
-  const Instruction({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // kerjain di sini
-    return Text("Instruction");
-  }
-}
-
-class Comments extends StatelessWidget {
-  const Comments({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // kerjain di sini
-    return Text("Comments");
   }
 }
