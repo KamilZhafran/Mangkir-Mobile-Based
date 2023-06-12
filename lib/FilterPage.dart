8ae32e94a57b0a06e93892b754bf1f882cf58341
BgsHryd <76993913+BgsHryd@users.noreply.github.com>
2023-06-12 13:31:04 +0700

    Filter, search and add resultPage
    1. FilterPage bismillah done
    2. resultpage juga udah I think

--- lib/FilterPage.dart
+++ lib/FilterPage.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tubes_app/HomePage.dart';
+import 'package:tubes_app/SearchResPage.dart';
+import 'model/Recipe.dart';
+import 'package:http/http.dart' as http;
 
 class FilterPage extends StatefulWidget {
   const FilterPage({Key? key}) : super(key: key);
@@ -9,8 +15,10 @@ class FilterPage extends StatefulWidget {
 }
 
 class _FilterPageState extends State<FilterPage> {
-  String? selectedFoodCategory;
-  String? selectedCookingTimeCategory;
+  String? selectedFoodCategory = "";
+  String? selectedCookingTimeCategory = "";
+
+  TextEditingController keywordController = TextEditingController();
 
   final List<String> foodCategories = [
     'Daging',
@@ -37,6 +45,7 @@ class _FilterPageState extends State<FilterPage> {
           title: SizedBox(
             width: 250,
             child: TextField(
+              controller: keywordController,
               maxLines: 1,
               textAlignVertical: TextAlignVertical.bottom,
               decoration: InputDecoration(
@@ -79,7 +88,7 @@ class _FilterPageState extends State<FilterPage> {
                       if (value != null && value) {
                         selectedFoodCategory = category;
                       } else {
-                        selectedFoodCategory = null;
+                        selectedFoodCategory = "";
                       }
                     });
                   },
@@ -118,9 +127,18 @@ class _FilterPageState extends State<FilterPage> {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Navigator.pop(context);
+            // Navigator.pop(context);
+            String keyword = keywordController.text;
+            print('inputted keyword: $keyword');
             print('Selected food category: $selectedFoodCategory');
             print('Selected cooking time category: $selectedCookingTimeCategory');
+
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) => SearchResPage(
+                keyword: keyword, 
+                kategori: selectedFoodCategory.toString(), 
+                durasi: selectedCookingTimeCategory.toString())
+            ));
           },
           child: Icon(Icons.check),
           backgroundColor: Color(0xFF43AA8B),
---
+++ lib/SearchResPage.dart
@@ -0,0 +1,156 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'model/Recipe.dart';
+import 'package:http/http.dart' as http;
+import 'FilterPage.dart';
+
+
+
+Future<List<Recipe>> searchRecipe(String keyword, String kategori, String durasi) async {
+  final Map<String, String> headers = {'Content-Type': 'application/json'};
+    final uri = Uri.parse('http://192.168.0.110:8000/api/search')
+      .replace(queryParameters: {
+    'keyword': keyword,
+    'durasi': durasi,
+    'category': kategori,
+  });
+  final res = await http.get(uri, headers: headers);
+  if (res.statusCode == 200) {
+    var data = jsonDecode(res.body);
+    var parsed = data.cast<Map<String, dynamic>>();
+    return parsed.map<Recipe>((json) => Recipe.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed');
+  }
+}
+
+class SearchResPage extends StatefulWidget {
+  final String keyword;
+  final String kategori;
+  final String durasi;
+
+  const SearchResPage({
+    required this.keyword,
+    required this.kategori,
+    required this.durasi,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _SearchResPageState createState() => _SearchResPageState();
+}
+
+class _SearchResPageState extends State<SearchResPage>{
+  late Future<List<Recipe>> recipes;
+
+  @override
+  void initState() {
+    super.initState();
+    print([widget.keyword, widget.kategori, widget.durasi]);
+    recipes = searchRecipe(widget.keyword, widget.kategori, widget.durasi);
+  }
+
+@override
+Widget build(BuildContext context) {
+  return SafeArea(
+    child: Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SizedBox(
+            width: 300,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FilterPage()),
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+                    const SizedBox(width: 10),
+                    Text(
+                      'Cari Resep...',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: FutureBuilder<List<Recipe>>(
+        future: recipes,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final recipeList = snapshot.data!;
+            return ListView.builder(
+              itemCount: recipeList.length,
+              itemBuilder: (context, index) {
+                final recipe = recipeList[index];
+                return GestureDetector(
+                  onTap: () {
+                    // nanti disini pindah
+                    print(recipe.id);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: Colors.blue, // Replace with your desired color or image
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.restaurant_menu_sharp,
+                          color: Colors.white,
+                        ),
+                      ),
+                      title: Text(
+                        recipe.judul,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Kategori: ${recipe.kategori}'),
+                          Text('Durasi: ${recipe.durasi}'),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text('No data found'));
+          }
+        },
+      ),
+    ),
+  );
+}
+
+}
